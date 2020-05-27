18edcf350647839e84727b529f76325e6b80b2a1
Elle O'Brien <andronovhopf@gmail.com>
2020-05-27 13:06:48 -0700

    Update train.py

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ y_test = np.genfromtxt("data/test_labels.csv")
 
 
 # Fit a model
-clf = MLPClassifier(hidden_layer_sizes=(100,))
+clf = MLPClassifier(hidden_layer_sizes=(10,))
 clf.fit(X_train,y_train)
 
 # Get overall accuracy
